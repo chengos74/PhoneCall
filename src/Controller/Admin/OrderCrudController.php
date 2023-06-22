b00ea149d6013463366ece4249f10ee2c32fd53f
Cheng <chengkoeng@gmail.com>
2023-06-22 22:57:50 +0200

    new update

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Admin;
 
 use App\Entity\Order;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Controller\Admin\OrderCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
