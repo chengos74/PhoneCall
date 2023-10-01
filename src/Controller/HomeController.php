b33625045053d071f5ec466cce30cee4e3eec490
Cheng <chengkoeng@gmail.com>
2023-10-01 23:54:15 +0200

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -23,30 +23,20 @@ class HomeController extends AbstractController
     public function index(Cart $cart): Response
     {
 
-        // $cartComplete = [];
-        // if ($cart->get()) {
-        //     foreach ($cart->get() as $id => $quantity) { //$id = key $quantity = value
-        //         $cartComplete[] = [
-        //             'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
-        //             'quantity' => $quantity
-        //         ];
-        //     }
-        // }
+
         $cartComplete = $cart->getFull();
 
         $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
-        // dd($products);
+
 
         $headers = $this->entityManager->getRepository(Header::class)->findAll();
-        // dd($headers);
 
-        // dd($cartComplete);
 
         return $this->render('home/home.html.twig', [
             'cart' => $cartComplete,
             'products' => $products,
             'headers' => $headers
         ]);
-        // return $this->render('home/home.html.twig');
+
     }
 }
